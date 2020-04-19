c7fbcd0fcc0d1101c18bc0f68e7bd1321d3025da
Fabricio <fabcovalesci@gmail.com>
2020-04-19 02:23:39 -0300

    configurado wsgi.py

--- app/wsgi.py
+++ app/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings.dev')
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
